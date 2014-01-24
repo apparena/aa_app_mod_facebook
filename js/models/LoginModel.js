54bb1aa6445ea98f15d5b8200fba258fcf71f241
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-24 10:22:39 +0100

    GNR-2 updated localstorage scope

--- js/models/LoginModel.js
+++ js/models/LoginModel.js
@@ -10,7 +10,7 @@ define([
         Model.namespace = 'facebookLoginModel';
 
         Model.code = Backbone.Model.extend({
-            localStorage: new Backbone.LocalStorage('AppArenaAdventskalenderApp_' + _.aa.instance.i_id + '_FbLoginData'),
+            localStorage: new Backbone.LocalStorage('aa_app_mod_facebook_' + _.aa.instance.i_id + '_FbLoginData'),
 
             defaults: {
                 'id':         1,
